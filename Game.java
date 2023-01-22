d3d9c5488b4aafb5953ab57f4f77c1a76da403a5
pquaidoo <pquaid@uw.edu>
2023-01-22 13:25:40 -0800

    commit and push test2

--- Game.java
+++ Game.java
@@ -1,5 +1,5 @@
 public class Game {
     public static void main(String[] args) {
-        System.out.println("the game is kinda fire ngl");
+        System.out.println("nope it still sucks");
     }
 }
