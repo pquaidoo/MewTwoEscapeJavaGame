c6350ea06625528008127dc73b60b867a6f9b427
YoansOmega <yonaskebedeomega@gmail.com>
2023-02-08 08:31:26 -0800

    Did part 7, error in SuperObject class with importing the GamePanel to the class and tileSize not being public.

---
+++ TPP Game Project/src/Object/OBJ_Chest.java
@@ -0,0 +1,16 @@
+package Object;
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
+
+public class OBJ_Chest extends SuperObject {
+
+    public OBJ_Chest() {
+        name = "Chest";
+        try {
+            image = ImageIO.read(getClass().getResourceAsStream("/Objects/chest.png"));
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ TPP Game Project/src/Object/SuperObject.java
@@ -0,0 +1,27 @@
+package Object;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import GamePanel;
+public class SuperObject {
+
+    public BufferedImage image;
+    public String name;
+    public boolean collision = false;
+    public int worldX, worldY;
+
+    public void draw(Graphics2D g2, GamePanel gp) {
+
+        int screenX = worldX - gp.player.worldX + gp.player.screenX;
+        int screenY = worldY - gp.player.worldY + gp.player.screenY;
+
+
+        if (worldX + gp.tileSize > gp.player.worldX - gp.player.screenX &&
+            worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
+            worldY + gp.tileSize > gp.player.worldY - gp.player.screenY &&
+            worldY - gp.tileSize < gp.player.worldY + gp.player.screenY) {//doesn't display pixels if its within camera
+
+            g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
+        }
+    }
+
+}
